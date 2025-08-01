2a1b929d42921c8c4c9911e7c5edb572aaf38309
Mateusz Grzybowski <mateusz.f.grzybowski@gmail.com>
2025-08-01 13:29:06 +0200

    Include testing in docker

--- tests/test_docs.rs
+++ tests/test_docs.rs
@@ -1,9 +1,8 @@
-use lopdf::{Document, Object, ObjectId, Stream, Bookmark, Dictionary};
+use lopdf::Document;
 use rust_cli::docs::convert_pdf_to_text;
-use lopdf::dictionary;
 
 
-fn generate_mock_document() -> Document {
+fn load_mock_document() -> Document {
     let doc = Document::load("tests/fixtures/mock_pdf.pdf").unwrap();
     doc
 }
@@ -11,7 +10,7 @@ fn generate_mock_document() -> Document {
 
 #[test]
 fn test_convert_pdf_to_text() -> () {
-    let mock_document = generate_mock_document();
+    let mock_document = load_mock_document();
     let result = convert_pdf_to_text(&mock_document);
     assert_eq!(result.len(), 12);
 }

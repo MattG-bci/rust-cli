fc4cff3ed6a546e4519e19b778205e6dfc90988c
Mateusz Grzybowski <mateusz.f.grzybowski@gmail.com>
2025-08-05 22:27:04 +0100

    Create dir to store htmls on a dly

--- src/docs.rs
+++ src/docs.rs
@@ -3,6 +3,10 @@ use std;
 
 
 pub fn convert_pdf_to_html(path_to_file: &String) -> std::process::Output {
+    if !std::path::Path::new("./src/.html").exists() {
+        std::fs::create_dir("./src/.html").unwrap();
+    }
+
     let cmd = std::process::Command::new("pdftohtml")
         .arg(path_to_file)
         .arg("./src/.html/output.html")

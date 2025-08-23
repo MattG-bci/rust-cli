1501fa416a03436b8e1318766bd01a37e0881ff4
Mateusz Grzybowski <mateusz.f.grzybowski@gmail.com>
2025-08-23 21:18:07 +0100

    Add tests

---
+++ tests/test_api.rs
@@ -0,0 +1,33 @@
+
+
+#[cfg(test)]
+mod test_client {
+    use rust_cli::api;
+    use std::env;
+    use serial_test::serial;
+
+    #[should_panic]
+    #[test]
+    #[serial]
+    fn test_obsidian_client_unsupported_client_type() {
+        env::set_var("CLIENT_TYPE", "notion");
+        api::client::Client::new();
+    }
+
+    #[test]
+    #[serial]
+    fn test_obsidian_client_supported_client_types() {
+        let client_types: [&str; 2] = ["obsidian", "local"];
+        for client_type in client_types {
+            env::set_var("CLIENT_TYPE", client_type);
+            api::client::Client::new();
+        }
+    }
+
+}
+
+
+
+
+
+
